197db8a3caae369030abb691f95cc7d542003925
luddwichr <luddwichr@users.noreply.github.com>
2018-11-18 23:10:24 +0100

    use S3ObjectId

--- src/main/java/com/example/awsexperiments/GetObjectRequestFactory.java
+++ src/main/java/com/example/awsexperiments/GetObjectRequestFactory.java
@@ -2,13 +2,19 @@ package com.example.awsexperiments;
 
 import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord;
 import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3ObjectId;
 
 public class GetObjectRequestFactory {
 
 	public GetObjectRequest getObjectRequest(S3EventNotificationRecord record) {
-		// TODO: Law of demeter is violated here -> introduce some additional class to handle information retrieval
+		S3ObjectId s3ObjectId = getS3ObjectId(record);
+		return new GetObjectRequest(s3ObjectId);
+	}
+
+	private S3ObjectId getS3ObjectId(S3EventNotificationRecord record) {
+		// TODO: Law of demeter is violated here -> introduce some additional class to extract S3ObjectId
 		String s3Key = record.getS3().getObject().getUrlDecodedKey();
 		String s3Bucket = record.getS3().getBucket().getName();
-		return new GetObjectRequest(s3Bucket, s3Key);
+		return new S3ObjectId(s3Key, s3Bucket);
 	}
 }
