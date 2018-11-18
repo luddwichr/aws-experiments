405d163de652bf788987c03a4e121c59bbece7c4
luddwichr <luddwichr@users.noreply.github.com>
2018-11-18 23:07:42 +0100

    use getUrlDecodedKey() instead of getKey()

--- src/main/java/com/example/awsexperiments/GetObjectRequestFactory.java
+++ src/main/java/com/example/awsexperiments/GetObjectRequestFactory.java
@@ -6,15 +6,8 @@ import com.amazonaws.services.s3.model.GetObjectRequest;
 public class GetObjectRequestFactory {
 
 	public GetObjectRequest getObjectRequest(S3EventNotificationRecord record) {
-		// AWS code uses the following approach: to be evaluated which one is correct
-		// (https://docs.aws.amazon.com/lambda/latest/dg/with-s3-example-deployment-pkg.html)
-		// Object key may have spaces or unicode non-ASCII characters.
-		//String srcKey = record.getS3().getObject().getKey()
-		//		.replace('+', ' ');
-		// srcKey = URLDecoder.decode(srcKey, "UTF-8");
-
 		// TODO: Law of demeter is violated here -> introduce some additional class to handle information retrieval
-		String s3Key = record.getS3().getObject().getKey();
+		String s3Key = record.getS3().getObject().getUrlDecodedKey();
 		String s3Bucket = record.getS3().getBucket().getName();
 		return new GetObjectRequest(s3Bucket, s3Key);
 	}
