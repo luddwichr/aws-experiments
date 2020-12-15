b3b192a40a16ab58f2fa45d611616b24b0c1837b
luddwichr <luddwichr@users.noreply.github.com>
2020-12-15 11:13:33 +0100

    use aws-lambda-java-tests to simulate s3 event

--- src/test/java/com/example/awsexperiments/DemoLambdaTest.java
+++ src/test/java/com/example/awsexperiments/DemoLambdaTest.java
@@ -3,17 +3,17 @@ package com.example.awsexperiments;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification.S3EventNotificationRecord;
+import com.amazonaws.services.lambda.runtime.tests.annotations.Event;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
-import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -37,9 +37,10 @@ class DemoLambdaTest {
 	@Mock
 	private S3ObjectMapper s3ObjectMapper;
 
-	@Test
-	void handleRequest() throws IOException {
-		S3EventNotificationRecord record = mock(S3EventNotificationRecord.class);
+	@ParameterizedTest
+	@Event(value = "s3-event.json", type = S3Event.class)
+	void handleRequest(S3Event event) throws IOException {
+		S3EventNotificationRecord record = event.getRecords().stream().findFirst().orElseThrow(IllegalStateException::new);
 		GetObjectRequest getObjectRequest = mock(GetObjectRequest.class);
 		when(getObjectRequestFactory.getObjectRequest(record)).thenReturn(getObjectRequest);
 		S3Object s3Object = mock(S3Object.class);
@@ -47,8 +48,7 @@ class DemoLambdaTest {
 		SomeDto someDto = new SomeDto("test");
 		when(s3ObjectMapper.map(s3Object, SomeDto.class)).thenReturn(someDto);
 
-		S3Event s3Event = new S3Event(Collections.singletonList(record));
-		Void response = demoLambda.handleRequest(s3Event, context);
+		Void response = demoLambda.handleRequest(event, context);
 
 		assertThat(response).isNull();
 	}
