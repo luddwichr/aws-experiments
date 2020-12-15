3525523b621b81091a44c192c11e8fdad2dbee23
luddwichr <luddwichr@users.noreply.github.com>
2020-12-15 11:30:17 +0100

    get rid of GetOjectRequest indirection
    according to the docs GetOjectRequest usage via s3Client.getObject() is vulnerable to HTTP connection congestion. Probably only worth using it if the s3 object contents are extremely large and consuming them as stream instead of a string is more performant.

--- src/main/java/com/example/awsexperiments/DemoLambda.java
+++ src/main/java/com/example/awsexperiments/DemoLambda.java
@@ -3,31 +3,32 @@ package com.example.awsexperiments;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification;
 import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification.S3EventNotificationRecord;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectId;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 
 public class DemoLambda implements RequestHandler<S3Event, Void> {
 
 	private static final Logger logger = LogManager.getLogger(DemoLambda.class);
 	private final AmazonS3 s3Client;
-	private final GetObjectRequestFactory getObjectRequestFactory;
-	private final S3ObjectMapper s3ObjectMapper;
+	private final ObjectMapper objectMapper;
 
 	@SuppressWarnings("unused")
 	public DemoLambda() {
-		this( AmazonS3ClientBuilder.defaultClient(), new GetObjectRequestFactory(), new S3ObjectMapper());
+		this(AmazonS3ClientBuilder.defaultClient(), new ObjectMapper());
 	}
 
-	private DemoLambda(AmazonS3 s3Client, GetObjectRequestFactory getObjectRequestFactory, S3ObjectMapper s3ObjectMapper) {
+	private DemoLambda(AmazonS3 s3Client, ObjectMapper objectMapper) {
 		this.s3Client = s3Client;
-		this.getObjectRequestFactory = getObjectRequestFactory;
-		this.s3ObjectMapper = s3ObjectMapper;
+		this.objectMapper = objectMapper;
 	}
 
 	@Override
@@ -37,13 +38,21 @@ public class DemoLambda implements RequestHandler<S3Event, Void> {
 	}
 
 	private void handleRecord(S3EventNotificationRecord record) {
-		S3Object object = s3Client.getObject(getObjectRequestFactory.getObjectRequest(record));
+		S3ObjectId objectId = getS3ObjectId(record);
+		String objectAsString = s3Client.getObjectAsString(objectId.getBucket(), objectId.getKey());
 		try {
-			SomeDto someDto  = s3ObjectMapper.map(object, SomeDto.class);
+			SomeDto someDto = objectMapper.readValue(objectAsString, SomeDto.class);
 			logger.info(someDto.toString());
 		} catch (IOException e) {
-			logger.error("failed to process input stream!" + e.getMessage());
+			throw new UncheckedIOException(e);
 		}
+	}
+
+	private S3ObjectId getS3ObjectId(S3EventNotificationRecord record) {
+		S3EventNotification.S3Entity s3 = record.getS3();
+		String s3Key = s3.getObject().getUrlDecodedKey();
+		String s3Bucket = s3.getBucket().getName();
+		return new S3ObjectId(s3Bucket, s3Key);
 	}
 
 }
--- src/test/java/com/example/awsexperiments/DemoLambdaTest.java
+++ src/test/java/com/example/awsexperiments/DemoLambdaTest.java
@@ -2,11 +2,9 @@ package com.example.awsexperiments;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
-import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification.S3EventNotificationRecord;
 import com.amazonaws.services.lambda.runtime.tests.annotations.Event;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.mockito.InjectMocks;
@@ -16,7 +14,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -32,21 +29,15 @@ class DemoLambdaTest {
 	private AmazonS3 s3Client;
 
 	@Mock
-	private GetObjectRequestFactory getObjectRequestFactory;
-
-	@Mock
-	private S3ObjectMapper s3ObjectMapper;
+	private ObjectMapper objectMapper;
 
 	@ParameterizedTest
 	@Event(value = "s3-event.json", type = S3Event.class)
 	void handleRequest(S3Event event) throws IOException {
-		S3EventNotificationRecord record = event.getRecords().stream().findFirst().orElseThrow(IllegalStateException::new);
-		GetObjectRequest getObjectRequest = mock(GetObjectRequest.class);
-		when(getObjectRequestFactory.getObjectRequest(record)).thenReturn(getObjectRequest);
-		S3Object s3Object = mock(S3Object.class);
-		when(s3Client.getObject(getObjectRequest)).thenReturn(s3Object);
+		String objectAsString = "object as string";
+		when(s3Client.getObjectAsString("example-bucket", "test/key")).thenReturn(objectAsString);
 		SomeDto someDto = new SomeDto("test");
-		when(s3ObjectMapper.map(s3Object, SomeDto.class)).thenReturn(someDto);
+		when(objectMapper.readValue(objectAsString, SomeDto.class)).thenReturn(someDto);
 
 		Void response = demoLambda.handleRequest(event, context);
 
